c67d0a2f5ac8fbfae2101468119196d2c5d20bb5
krevkym <44412598+krevkym@users.noreply.github.com>
2020-10-15 21:57:41 +0200

    Add files via upload

---
+++ Factory.java
@@ -0,0 +1,9 @@
+package semestralka;
+
+public class Factory {
+    private int[][] production;
+
+    public void setProduction(int[][] production) {
+        this.production = production;
+    }
+}
